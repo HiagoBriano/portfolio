91e5609120a51a13719589fff5bcd6693897b535
HiagoBriano <hiago.artist@hotmail.com>
2021-12-24 19:32:33 -0300

    adicionei o link dos projetos

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,11 @@ export default class Footer extends Component {
   render() {
     return (
       <section className="footer">
-        <p>Feito e constantemente atualizado com React, JavaScript, CSS e 💙</p>
+        <div>
+          <p>
+            Feito e constantemente atualizado com React, JavaScript, CSS e 💙
+          </p>
+        </div>
       </section>
     );
   }
--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -2,59 +2,44 @@ import { Component } from 'react';
 import './Portfolio.css';
 
 import Imagem1 from './Images/1.png';
-import Imagem2 from './Images/2.png';
-import Imagem3 from './Images/3.png';
-import Imagem4 from './Images/4.png';
-import Imagem5 from './Images/5.png';
-import Imagem6 from './Images/6.png';
+import Imagem2 from './Images/2.gif';
+import Imagem3 from './Images/3.gif';
+import Imagem4 from './Images/4.gif';
 
 export default class Portfolio extends Component {
   render() {
     const fotos = [
       {
-        nome: 'Imagem 1',
-        fonte: Imagem1,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 2',
-        fonte: Imagem2,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 3',
+        nome: 'Front End Online Store',
         fonte: Imagem3,
-        feito: '24/12/2021',
+        feito: '18/11/2021',
+        web: 'https://hiagobriano.github.io/projeto_front_end_online_store/',
+        codigo: 'https://github.com/HiagoBriano/projeto_front_end_online_store',
       },
       {
-        nome: 'Imagem 4',
+        nome: 'Projeto Trybewarts',
+        fonte: Imagem1,
+        feito: '24/09/2021',
+        web: 'https://hiagobriano.github.io/projeto_trybewarts/',
+        codigo:
+          'https://github.com/HiagoBriano/HiagoBriano.github.io/tree/master/projeto_trybewarts',
+      },
+
+      {
+        nome: 'Projeto Todo List',
+        fonte: Imagem2,
+        feito: '13/09/2021',
+        web: 'https://hiagobriano.github.io/projeto_todo_list/',
+        codigo:
+          'https://github.com/HiagoBriano/HiagoBriano.github.io/tree/master/projeto_todo_list',
+      },
+      {
+        nome: 'Pixels Art',
         fonte: Imagem4,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 5',
-        fonte: Imagem5,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 6',
-        fonte: Imagem6,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 1',
-        fonte: Imagem1,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 2',
-        fonte: Imagem2,
-        feito: '24/12/2021',
-      },
-      {
-        nome: 'Imagem 3',
-        fonte: Imagem3,
-        feito: '24/12/2021',
+        feito: '10/09/2021',
+        web: 'https://hiagobriano.github.io/projeto_pixels_art/',
+        codigo:
+          'https://github.com/HiagoBriano/HiagoBriano.github.io/tree/master/projeto_pixels_art',
       },
     ];
 
@@ -62,13 +47,24 @@ export default class Portfolio extends Component {
       <section className="portfolio" id="portfolio">
         <h3 className="portfolio--h3">Portfólio</h3>
         <div className="portfolio--imagens">
-          {fotos.map(({ fonte, nome }) => (
-            <img
-              src={fonte}
-              alt={nome}
-              key={nome}
-              className="portfolio--imagem"
-            />
+          {fotos.map(({ fonte, nome, web, codigo }) => (
+            <div className="portfolio--divImagem">
+              <img
+                src={fonte}
+                alt={nome}
+                key={nome}
+                className="portfolio--imagem"
+              />
+              <div className="portfolio--linkImagem">
+                <a href={codigo} rel="noreferrer" target="_blank">
+                  Código do projeto
+                </a>
+
+                <a href={web} rel="noreferrer" target="_blank">
+                  Site na Web
+                </a>
+              </div>
+            </div>
           ))}
         </div>
       </section>
